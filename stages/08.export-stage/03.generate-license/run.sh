826edcd0a7009640871b1ae53fa596ae95dba904
F5OEO <evaristec@gmail.com>
2025-09-13 22:43:18 +0200

    Skip licence for now

--- stages/08.export-stage/03.generate-license/run.sh
+++ stages/08.export-stage/03.generate-license/run.sh
@@ -6,17 +6,17 @@
 # Copyright (c) 2025 Analog Devices, Inc.
 # Author: Larisa Radu <larisa.radu@analog.com>
 
-mkdir -p kuiper-volume/licensing
-mkdir -p ${BUILD_DIR}/licensing/copyright
-mount --bind kuiper-volume/licensing ${BUILD_DIR}/licensing
+#mkdir -p kuiper-volume/licensing
+#mkdir -p ${BUILD_DIR}/licensing/copyright
+#mount --bind kuiper-volume/licensing ${BUILD_DIR}/licensing
 
-chroot "${BUILD_DIR}" << EOF
-	bash stages/08.export-stage/03.generate-license/run-chroot.sh
-EOF
+#chroot "${BUILD_DIR}" << EOF
+#	bash stages/08.export-stage/03.generate-license/run-chroot.sh
+#EOF
 
-cp -r stages/08.export-stage/03.generate-license/img/ kuiper-volume/licensing
+#cp -r stages/08.export-stage/03.generate-license/img/ kuiper-volume/licensing
 
-umount ${BUILD_DIR}/licensing
+#umount ${BUILD_DIR}/licensing
 
-rm -r "${BUILD_DIR}/stages"
-rm -r "${BUILD_DIR}/licensing"
+#rm -r "${BUILD_DIR}/stages"
+#rm -r "${BUILD_DIR}/licensing"
