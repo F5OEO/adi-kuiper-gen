615a2583f95493935bb5e733f1c25aef7c670bb1
F5OEO <evaristec@gmail.com>
2025-10-12 13:18:43 +0200

    Add serial to hostname

--- stages/05.adi-tools/01.install-libiio/files/setini.sh
+++ stages/05.adi-tools/01.install-libiio/files/setini.sh
@@ -13,5 +13,6 @@ echo "hw_model=$model" >> /etc/libiio.ini
 
 serial=$(ip link show eth0 | awk '/ether/ {print $2}')
 echo -e "hw_serial=$serial\n" >> /etc/libiio.ini
+hostnamectl set-hostname "zukaneoper-$serial"
 
 echo -e "fw_version=tezuka-0.1.8-2-g6f80\n" >> /etc/libiio.ini
