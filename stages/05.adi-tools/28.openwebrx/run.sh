0e151d3d35665465dd645bb870b898b6e5e1d86d
F5OEO <evaristec@gmail.com>
2025-10-12 13:28:22 +0200

    Try an other method to install openwebrx

--- stages/05.adi-tools/28.openwebrx/run.sh
+++ stages/05.adi-tools/28.openwebrx/run.sh
@@ -2,11 +2,11 @@ if [ "${CONFIG_OPENWEBRX}" = y ]; then
 
 
 chroot "${BUILD_DIR}" << EOF
-
-curl -s https://luarvique.github.io/ppa/openwebrx-plus.gpg | gpg --yes --dearmor -o /etc/apt/trusted.gpg.d/openwebrx-plus.gpg
-bash -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/openwebrx-plus.gpg] https://luarvique.github.io/ppa/bookworm ./" | tee /etc/apt/sources.list.d/openwebrx-plus.list'
-apt update
-apt install openwebrx
+cd /usr/local/src
+git clone https://github.com/luarvique/openwebrx.git
+cd openwebrx
+./buildall.sh
+dpkg -i ./owrx-output/*.deb
 EOF
 
 else
