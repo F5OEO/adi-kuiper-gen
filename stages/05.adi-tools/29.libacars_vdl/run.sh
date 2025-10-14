725e6d89d5f8f9bf9e5b04ad523139f6e1306148
F5OEO <evaristec@gmail.com>
2025-10-14 11:48:54 +0200

    Add ACARS

---
+++ stages/05.adi-tools/29.libacars_vdl/run.sh
@@ -0,0 +1,24 @@
+if [ "${CONFIG_ACARSVDL}" = y ]; then
+
+
+chroot "${BUILD_DIR}" << EOF
+cd /usr/local/src
+git clone https://github.com/szpajder/libacars.git
+cd libacars
+mkdir build && cd build
+cmake ../
+make install
+
+cd /usr/local/src
+git clone https://github.com/szpajder/dumpvdl2.git
+cd dumpvdl2
+mkdir build && cd build
+cmake ../
+make install
+
+EOF
+
+else
+	echo "acarsvdl won't be installed because CONFIG_ACARSVDL is set to 'n'."
+	
+fi
