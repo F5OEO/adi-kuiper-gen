91ba57cd5fd10621827a67bfcd3497f0b68d5c01
F5OEO <evaristec@gmail.com>
2025-10-15 09:14:12 +0200

    Fix tezuka tools and .config rights

--- stages/05.adi-tools/27.tezuka_tools/run.sh
+++ stages/05.adi-tools/27.tezuka_tools/run.sh
@@ -12,7 +12,7 @@ if [ "${CONFIG_TEZUKA_TOOLS}" = y ]; then
 cp -r "${BASH_SOURCE%%/run.sh}"/files/	"${BUILD_DIR}/home/analog/tools/"
 #mkdir -p "${BUILD_DIR}/home/analog/.config/Gpredict" ----> .config as root is a very bad IDEA, vnc is not working for example
 #cp "${BASH_SOURCE%%/run.sh}"/gpredict.cfg "${BUILD_DIR}/home/analog/.config/Gpredict/"
-
+install -d 666 "${BUILD_DIR}/home/analog/.config/Gpredict"
 install -m 666 "${BASH_SOURCE%%/run.sh}"/gpredict.cfg "${BUILD_DIR}/home/analog/.config/Gpredict/"
 #set anonymous
 cp "${BASH_SOURCE%%/run.sh}"/system.pa "${BUILD_DIR}/etc/pulse/"
@@ -23,7 +23,7 @@ systemctl --user disable pulseaudio.socket pulseaudio.service
 systemctl enable pulseaudio
 EOF
 else
-	echo "tezuka_tools won't be installed because CONFIG_TEUKA_TOOLS is set to 'n'."
+	echo "tezuka_tools won't be installed because CONFIG_TEZUKA_TOOLS is set to 'n'."
 fi
 
 
