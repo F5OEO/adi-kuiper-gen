0bfc303840d6eee80403a85c8ba24817a8cff699
F5OEO <evaristec@gmail.com>
2025-10-25 19:29:03 +0200

    Start installing remotesdr

---
+++ stages/05.adi-tools/30.remote-sdr-tezuka/run.sh
@@ -0,0 +1,14 @@
+if [ "${CONFIG_REMOTESDR}" = y ]; then
+
+
+chroot "${BUILD_DIR}" << EOF
+npm install -g express cors ejs onoff xmlrpc socket.io
+cd /usr/local/src
+git clone https://github.com/F5OEO/Remote-SDR-Tezuka 
+mv Remote-SDR-Tezuka/remsdr /remsdr
+EOF
+
+else
+	echo "remotesdr won't be installed because REMOTESDR is set to 'n'."
+	
+fi
