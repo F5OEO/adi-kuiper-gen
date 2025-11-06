699c94bacb2ea105a006871cec1c6030f35dd80c
F5OEO <evaristec@gmail.com>
2025-11-06 12:45:41 +0100

    Add ip fallback to 192.168.3.1 if no dhcp server

---
+++ stages/05.adi-tools/27.2.netplan/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# kuiper2.0 - Embedded Linux for Analog Devices Products
+#
+# Copyright (c) 2024 Analog Devices, Inc.
+# Author: Larisa Radu <larisa.radu@analog.com>
+
+
+if [ "${CONFIG_NETPLAN}" = y ]; then
+
+install -m 666 "${BASH_SOURCE%%/run.sh}/files/01.net.yaml" "${BUILD_DIR}/etc/netplan/"
+install -m 666 "${BASH_SOURCE%%/run.sh}/files/10-dhcp-fallback" "${BUILD_DIR}/usr/lib/networkd-dispatcher/routable.d/"
+install -m 666 "${BASH_SOURCE%%/run.sh}/files/isc-dhcp-server" "${BUILD_DIR}/etc/default/"
+install -m 666 "${BASH_SOURCE%%/run.sh}/files/dhcpd.conf" "${BUILD_DIR}/etc/dhcp/"
+
+chroot "${BUILD_DIR}" << EOF
+
+chmod +x /usr/lib/networkd-dispatcher/routable.d/10-dhcp-fallback
+
+systemctl enable systemd.networkd
+systemctl enable networkd-dispatcher
+systemctl enable isc-dhcp-server
+
+EOF
+else
+	echo "tezuka_netplan won't be installed because CONFIG_NETPLAN is set to 'n'."
+fi
+
+
+
+
