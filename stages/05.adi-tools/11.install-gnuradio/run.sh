6167434c0647ea05c6f8b13c070612a0daf81dfc
F5OEO <evaristec@gmail.com>
2025-11-26 10:54:12 +0100

    Fix Python pass for gnuradio

--- stages/05.adi-tools/11.install-gnuradio/run.sh
+++ stages/05.adi-tools/11.install-gnuradio/run.sh
@@ -11,6 +11,10 @@ if [ "${CONFIG_GNURADIO}" = y ]; then
 # Install Gnuradio 3.10.5
 chroot "${BUILD_DIR}" << EOF
 	apt-get install -y gnuradio gnuradio-dev gr-satellites --no-install-recommends
+	# Set the Python path to deb packages when launching GNURadio from both terminal and GUI
+	sed -i 's|^Exec=gnuradio-companion|Exec=env PYTHONPATH=/usr/lib/python3/dist-packages:\$PYTHONPATH gnuradio-companion|' /usr/share/applications/gnuradio-grc.desktop
+	echo "alias gnuradio-companion='PYTHONPATH=/usr/lib/python3/dist-packages:\$PYTHONPATH gnuradio-companion'" >> /etc/bash.bashrc
+
 	cd /usr/local/src
 
 	# Clone papr
