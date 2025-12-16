59466bf0c52d8eff55f4a910506ef3cae5835add
F5OEO <evaristec@gmail.com>
2025-12-16 14:10:29 +0100

    Bumped to tezuka 0.2.3

--- stages/06.boot-partition/03.tezuka-boot-files/run.sh
+++ stages/06.boot-partition/03.tezuka-boot-files/run.sh
@@ -7,7 +7,7 @@
 # Author: Larisa Radu <larisa.radu@analog.com>
 
 SERVER="https://github.com/F5OEO/tezuka_fw/releases/download"
-VERSION="0.2.2"
+VERSION="0.2.3"
 TARGET_BOOT="${BUILD_DIR}"/usr/local/src/boot
 
 if [ "${CONFIG_TEZUKA_BOOT_FILES}" = y ]; then
